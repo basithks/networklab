d1f8c9e2f5f9b21acc6392841cf39e02c290c348
basithks <abdulbasith0485@gmail.com>
2025-05-21 15:57:49 +0530

    Add files via upload
    filess

---
+++ armstrong.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+echo "enter a number:"
+read num
+original=$num
+
+sum=0
+
+while [ $num -gt 0 ]
+do
+    digit=$(( num % 10 ))
+    cube=$(( digit * digit * digit ))
+    sum=$(( sum + cube ))
+    num=$(( num / 10 ))
+done
+
+if [ $sum -eq $original ]; then
+    echo "$original is an Armstrong number."
+else
+    echo "$original is not an Armstrong number."
+fi
---
+++ palindrome.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+echo "enter a number"
+read num
+original=$num
+
+reverse=0
+
+while [ $num -gt 0 ]
+do
+    remainder=$(( num % 10 ))
+    reverse=$(( reverse * 10 + $remainder ))
+    num=$(( num / 10 ))
+done
+
+if [ $original -eq $reverse ]; then
+    echo "$original is a palindrome."
+else
+    echo "$original is not a palindrome."
+fi
